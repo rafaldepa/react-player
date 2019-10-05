55bb155959d893e37bf66564e21eadf3b814d61f
sirmovie <kontakt@rafaldepa.pl>
2019-10-05 11:42:37 +0200

    CSS padding change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const StyledWrapper = styled.div`
   display: flex;
   width: 100%;
   min-height: 100vh;
-  padding: 100px;
+  padding: 30px;
   justify-content: center;
   align-items: center;
   box-sizing: border-box;
@@ -16,7 +16,7 @@ const StyledWrapper = styled.div`
 const StyledWrapperContent = styled.div`
   display: block;
   width: 100%;
-  max-width: 800px;
+  max-width: 900px;
   box-shadow: 0 20px 30px rgba(0, 0, 0, 0.2);
 `;
 
